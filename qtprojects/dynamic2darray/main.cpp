7215d50bf254b292af34aadcd7a30b090decfa45
joshuacamacho <joshuajcamacho@gmail.com>
2014-09-03 11:47:02 -0700

    Signed-off-by: joshuacamacho <joshuajcamacho@gmail.com>

--- qtprojects/dynamic2darray/main.cpp
+++ qtprojects/dynamic2darray/main.cpp
@@ -1,10 +1,16 @@
+/* Joshua Camacho
+ * 09/03/14
+ * CIS-17B-48597
+ * Review Assignment 1
+ */
 #include <Arry.cpp>
 #include <iostream>
 #include <QApplication>
 #include <QLabel>
+
 int main(int argc, char** argv){
     QApplication app(argc,argv);
-    Arry test(10,10);
+    Arry test(3,3);
     QLabel *label=new QLabel(test.toString());
     label->show();
     return app.exec();
